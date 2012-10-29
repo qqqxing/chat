328e36d12d42cdcac81c95975513b30a78fcbc81
qqqxing <qqqxing@gmail.com>
2012-10-29 22:02:20 +0800

    first blood

---
+++ chat/chatwrite.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once 'config.php';
+
+class WriteResult{
+	public $error;
+}
+
+if ( !isset($_COOKIE['userid']) || $_COOKIE['userid'] == ""){
+	$out = new WriteResult();
+	$out->error = 1;
+	print json_encode($out);
+	die();
+}
+
+
+if (isset($_GET['content']) && $_GET['content'] != ""){
+
+
+
+	$content = $_GET['content'];
+	$user = $_COOKIE['userid'];
+	$datetime = date("Y-m-d H:i:s");
+
+	$mysqli = new mysqli($db_host,$db_user,$db_pw,$db_name);
+
+	$out = new WriteResult();
+
+
+	if (!$mysqli){
+		$out->error = 1;
+		print json_encode($out);
+		die();
+	}
+
+	$query = "insert into chatlogs(user,content,datetime) values(?,?,?)";
+
+
+	$bind_result = $mysqli->prepare($query);
+	$bind_result->bind_param("sss",$user,$content,$datetime);
+	$bind_result->execute();
+	$bind_result->close();
+	$mysqli->close();
+
+	$out->error = 0;
+	print json_encode($out);
+
+}else{
+	$out = new WriteResult();
+	$out->error = 2;
+	print json_encode($out);
+	die();
+}
+
+
+
+
+
+?>
---
+++ chat/config.php
@@ -0,0 +1,9 @@
+<?php
+
+
+	$db_host = "127.0.0.1";
+	$db_user = "root";
+	$db_pw = "5228173";
+	$db_name = "pchat";
+
+?>
